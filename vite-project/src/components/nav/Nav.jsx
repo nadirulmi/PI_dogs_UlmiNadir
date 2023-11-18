27b3310d0ce984be2e4a450ee372989e71a51170
ulminadir <ulminadir99@gmail.com>
2023-11-18 01:59:55 -0300

    se comienza css responsive

--- vite-project/src/components/nav/Nav.jsx
+++ vite-project/src/components/nav/Nav.jsx
@@ -1,22 +1,44 @@
-import { Button } from "../button/Button"
-import style from "./Nav.module.css"
-import wikidog from "./img/wikidog.png"
+import { Button } from "../button/Button";
+import style from "./Nav.module.css";
+import wikidog from "./img/wikidog.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
+export const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-export const Nav = () =>{
-    
-    return(
-        <div>
-            <nav className={style.navBar}>
-                <Link to={"/dogs"}>
-                <img src={wikidog} alt="wiki dog logo"></img>
-                </Link>
-                <Button path="/dogs" text="Home"/>
-                <Button path="/create" text="Create your dog"/>
-                <Button path="/curiosities" text="Curiosities"/>
-                <Button path="/" text="Back to landing page"/>
-            </nav>
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className={style.navContainer}>
+      <nav>
+        <Link to={"/dogs"}>
+          <img src={wikidog} alt="wiki dog logo" />
+        </Link>
+        <div className={style.menu} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
-    )
-}
+        <ul className={menuOpen ? `${style.open}` : ""}>
+          <li>
+            <Button path="/dogs" text="Home" />
+          </li>
+          <li>
+            <Button path="/create" text="Create your dog" />
+          </li>
+          <li>
+            <Button path="/curiosities" text="Curiosities" />
+          </li>
+          <li>
+            <Button path="/" text="Back to landing page" />
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
