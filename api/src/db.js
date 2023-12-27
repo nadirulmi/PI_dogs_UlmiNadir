85c77c20e2b81e696214969482302084fbc43596
ulminadir <ulminadir99@gmail.com>
2023-12-27 14:57:30 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -12,13 +12,23 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 //   }
 // );
 
-const sequelize = new Sequelize(
-  DB_DEPLOY,
-  {
-    logging: false, 
-    native: false, 
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false, 
+  native: false,
+  dialectOptions: {
+    ssl : {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-);
+});
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión exitosa');
+  })
+  .catch(err => {
+    console.error('Error de conexión:', err);
+  });
 
 const basename = path.basename(__filename);
 
