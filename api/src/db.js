2b43a4bc340bd2603051070fbdaf833bc1eadf56
ulminadir <ulminadir99@gmail.com>
2023-12-27 15:12:32 -0300

    deploy DATABASE

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, POSTGRES_URL } = process.env;
 
 // const sequelize = new Sequelize(
 //   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
@@ -12,13 +12,23 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 //   }
 // );
 
-const sequelize = new Sequelize(
-  DB_DEPLOY,
-  {
-    logging: false, 
-    native: false, 
+const sequelize = new Sequelize(POSTGRES_URL, {
+  logging: false, 
+  native: false,
+  dialectOptions: {
+    ssl : {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-);
+});
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión exitosa');
+  })
+  .catch(err => {
+    console.error('Error de conexión:', err);
+  });
 
 const basename = path.basename(__filename);
 
